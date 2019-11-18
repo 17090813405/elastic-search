526252e5d094c1d63da09c8289640c71766451c2
daizhichao <daizhichao@koalareading.com>
2019-11-18 17:56:50 +0800

    update

--- src/main/java/com/example/elasticsearch/controller/JestTest.java
+++ src/main/java/com/example/elasticsearch/controller/JestTest.java
@@ -11,9 +11,7 @@ import io.searchbox.indices.DeleteIndex;
 import io.searchbox.indices.mapping.GetMapping;
 import io.searchbox.indices.mapping.PutMapping;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
@@ -72,10 +70,10 @@ public class JestTest {
      * 全文搜索
      */
     public static void serach1() {
-        String query = "\"工程师\"";
+        String query = "工程师";
         try {
             SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            searchSourceBuilder.query(new QueryStringQueryBuilder(query).field("description").defaultOperator(Operator.AND));
+            searchSourceBuilder.query(QueryBuilders.queryStringQuery(query));
             //分页设置
             searchSourceBuilder.from(0).size(2);
             log.info("全文搜索查询语句:" + searchSourceBuilder.toString());
