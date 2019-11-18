97860e0f56e6aa4a0ac4ca8a080cb87fcecb4b63
daizhichao <daizhichao@koalareading.com>
2019-11-18 16:24:49 +0800

    update

--- src/main/java/com/example/elasticsearch/controller/JestTest.java
+++ src/main/java/com/example/elasticsearch/controller/JestTest.java
@@ -75,7 +75,7 @@ public class JestTest {
         String query = "工程师";
         try {
             SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            searchSourceBuilder.query(new QueryStringQueryBuilder(query).field(query));
+            searchSourceBuilder.query(new QueryStringQueryBuilder(query).field("description"));
             //分页设置
             searchSourceBuilder.from(0).size(2);
             log.info("全文搜索查询语句:" + searchSourceBuilder.toString());
