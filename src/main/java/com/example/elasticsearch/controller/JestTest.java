9b023d6cca62042edce8be5366b8bed82731802f
daizhichao <daizhichao@koalareading.com>
2019-11-18 14:26:15 +0800

    update

--- src/main/java/com/example/elasticsearch/controller/JestTest.java
+++ src/main/java/com/example/elasticsearch/controller/JestTest.java
@@ -70,10 +70,10 @@ public class JestTest {
      * 全文搜索
      */
     public static void serach1() {
-        String query = "工程师";
+        String query = "+工程师";
         try {
             SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            searchSourceBuilder.query(QueryBuilders.simpleQueryStringQuery(query));
+            searchSourceBuilder.query(QueryBuilders.queryStringQuery(query));
             //分页设置
             searchSourceBuilder.from(0).size(2);
             log.info("全文搜索查询语句:" + searchSourceBuilder.toString());
